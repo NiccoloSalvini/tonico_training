aaa3774fc4353422edcb7586a1dcf5c4fb764b87
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-06-22 18:48:14 +0200

    large commit

---
+++ data/db_init.R
@@ -0,0 +1,83 @@
+library(RSQLite, warn.conflicts = F, quietly = T)
+library(tibble, warn.conflicts = F, quietly = T)
+library(RPostgres, warn.conflicts = F, quietly = T)
+library(DBI, warn.conflicts = F, quietly = T)
+
+# Create a connection object with dbi
+conn <- dbConnect(RPostgres::Postgres(),
+                  dbname = "d3cmoca1tan7ho", 
+                  host='ec2-52-17-1-206.eu-west-1.compute.amazonaws.com', 
+                  port="5432", 
+                  user="fnijsgqvdsupca", 
+                  password="8f0ffe72e0468f2a318c70c6a0a70b56d3706ba82112a52fc2e2f97a341469cc")  
+
+
+# Create a query to prepare the tonico_training db table with additional 'uid', 'id',
+# & the 4 created/modified columns
+create_clients_info_query = "CREATE TABLE clients_info (
+  uid                             INT GENERATED ALWAYS AS IDENTITY,
+  client_name                     VARCHAR(100),
+  client_surname                  VARCHAR(100),
+  email                           VARCHAR(100),
+  city                            VARCHAR(100),
+  phone_number_prefix             NUMERIC,
+  phone_number                    NUMERIC,
+  sex                             TEXT,
+  birthday                        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  age                             NUMERIC,
+  PRIMARY KEY(uid)
+)"
+
+create_clients_measurements_query = "CREATE TABLE clients_measurements (
+  measurement_id                  INT GENERATED ALWAYS AS IDENTITY,
+  uid_client                      INT,
+  form_name                       VARCHAR(100),
+  form_surname                    VARCHAR(100),
+  measurement_day                 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  height                          NUMERIC,
+  weight                          NUMERIC,
+  body_fat                        NUMERIC,
+  fat                             NUMERIC,
+  body_muscle                     NUMERIC,
+  muscle                          NUMERIC,
+  PRIMARY KEY(measurement_id),
+  CONSTRAINT fk_measurement
+    FOREIGN KEY(uid_client) 
+      REFERENCES clients_info(uid)
+)"
+
+
+
+# dbExecute() executes a SQL statement with a connection object
+# Drop the table if it already exists
+dbExecute(conn, "DROP TABLE IF EXISTS clients_info CASCADE")
+dbExecute(conn, "DROP TABLE IF EXISTS clients_measurements CASCADE")
+# Execute the query created above
+dbExecute(conn, create_clients_info_query)
+dbExecute(conn, create_clients_measurements_query)
+
+# Read in the RDS file created in 'data_prep.R'
+dat <- readRDS("data/prepped/clients_info.RDS")
+
+# add uid column to the `dat` data frame
+# dat$uid <- uuid::UUIDgenerate(n = nrow(dat))
+
+# reorder the columns
+dat <- dat %>%
+  select(uid, everything())
+
+
+# Fill in the postgresql tables with the values from the RDS file
+DBI::dbWriteTable(
+  conn,
+  name = "clients_info",
+  value = dat,
+  overwrite = FALSE,
+  append = TRUE
+)
+
+# List tables to confirm 'mtcars' table exists
+dbListTables(conn)
+
+# disconnect from SQLite before continuing
+dbDisconnect(conn)
---
+++ data/etl_data.R
@@ -0,0 +1,54 @@
+library(dplyr, quietly = T, warn.conflicts = F)
+library(tidyr, quietly = T, warn.conflicts = F)
+library(tibble, quietly = T, warn.conflicts = F)
+library(here, quietly = T, warn.conflicts = F)
+library(readxl, quietly = T, warn.conflicts = F)
+library(janitor, quietly = T, warn.conflicts = F)
+library(forcats, quietly = T, warn.conflicts = F)
+library(rvest, quietly = T, warn.conflicts = F)
+library(lubridate, quietly = T, warn.conflicts = F)
+library(gender, quietly = T, warn.conflicts = F)
+
+city_url <- "https://citymonitor.ai/environment/where-are-largest-cities-britain-1404"
+uk_cities <- session(url = city_url) %>%
+  html_elements(css = "ol:nth-child(16) li") %>%
+  html_text2() %>%
+  as_tibble() %>%
+  separate(col = value, into = c("city", "population"), sep = " – ") %>%
+  mutate(population = parse_number(population)) %>%
+  slice_sample(weight_by = population, replace = T, n = 100) %>%
+  pull(city)
+
+
+clients_info <- read_excel(here("data", "provided", "fake_clients_info_data.xlsx")) %>%
+  add_column(city = uk_cities) %>%
+  separate(col = phone_number_prefix, into = c("phone_number_prefix", "other"), sep = " ") %>%
+  select(-c(sex, other, age)) %>%
+  mutate(
+    email = tolower(email),
+    birthday = dmy(birthday),
+    age = interval(start = birthday, end = today()) / duration(n = 1, unit = "years"),
+    age = floor(age),
+    phone_number_prefix = parse_number(phone_number_prefix),
+    phone_number = parse_number(phone_number),
+    years = year(birthday),
+    sex = ifelse(str_sub(client_name, -1) %in% c("a", "e", "y", "e"), "F", "M"),
+    sex = as_factor(sex)
+  ) %>% 
+  select(-years)
+# do(results = gender(.$client_names, years = .$years[1], method = "ssa")) %>%
+# do(bind_rows(.$results))
+
+# 
+# client_measurament = read_csv(here("data", "provided", "fake_bmi_data.csv")) %>% 
+#   clean_names() %>% 
+#   rename(height = height_inches, weight = weight_pounds) %>% 
+#   drop_na(bmi) %>% 
+#   mutate(height = height*2.54,
+#          weight = 0.4535*weight,
+#          ) %>% 
+#   sample_n(100)
+#
+
+saveRDS(clients_info, file = "data/prepped/clients_info.RDS")
+
